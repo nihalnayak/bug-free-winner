dceb7ca6d45c81c0d1a037f9d25f26a27c3bba3c
Nihal Nayak <nihalnayak@gmail.com>
2021-10-24 19:31:36 -0400

    init train scot

--- train_scot.py
+++ train_scot.py
@@ -4,3 +4,16 @@ import evals as evals
 import scot2 as sc
 import numpy as np
 import scanpy
+
+X=np.load("./data/scatac_feat.npy")
+y=np.load("./data/scrna_feat.npy")
+print("Dimensions of input datasets are: ", "X= ", X.shape, " y= ", y.shape)
+
+# initialize SCOT object
+scot=sc.SCOT(X, y)
+# call the alignment with l2 normalization
+# X_new, y_new = scot.align(k=50, e=0.0005,  normalize=True)
+x_new, y_new = scot.align(k=5, e=0.05,  normalize=True)
+
+fracs=evals.calc_domainAveraged_FOSCTTM(x_new, y_new[0])
+print("Average FOSCTTM score for this alignment with X onto Y is: ", np.mean(fracs))
