6f22211dec8b074154ffdb68586ac8cebacc83c2
Nihal Nayak <nihalnayak@gmail.com>
2021-11-20 17:55:31 -0500

    add: docstrings for dimensionality reduction function

--- dimensionality_reduction.py
+++ dimensionality_reduction.py
@@ -1,7 +1,16 @@
-from sklearn.decomposition import TruncatedSVD, PCA, SparsePCA
-
-
 def run_decomposition(algo, data1, data2, mod1_dim, mod2_dim):
+    """Wrapper to reduce the dimensions of model.
+
+    Args:
+        algo (object): dimensionality reduction method
+        data1 (object): modality 1
+        data2 (object): modality 2
+        mod1_dim (int): dimension of the first modalit
+        mod2_dim (int): dimension of the second modality
+
+    Returns:
+        tuple: tuple containing the reduced dimensions.
+    """
     print("using algorithm: ", algo)
     print("reducing dimensionality: mod1")
     # Do PCA on the input data
