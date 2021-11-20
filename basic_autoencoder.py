c80d6eaf83044e60781356c548570586c835f4c2
Nihal Nayak <nihalnayak@gmail.com>
2021-11-20 16:45:39 -0500

    fix: training of autoencoders

--- basic_autoencoder.py
+++ basic_autoencoder.py
@@ -42,7 +42,8 @@ class AutoEncoder(nn.Module):
 
 def train_epoch(model, optimizer, loss_fn, train_dataloader, device):
     train_loss = 0.0
-    for i, batch in enumerate(train_dataloader):
+    model.train()
+    for batch in tqdm.tqdm(train_dataloader):
         x = batch[0]
         x = x.to(device)
         x_prime = model(x)
@@ -59,8 +60,9 @@ def train_epoch(model, optimizer, loss_fn, train_dataloader, device):
 
 def compute_val_loss(model, loss_fn, val_dataloader, device):
     val_loss = 0
+    model.eval()
     with torch.no_grad():
-        for batch in val_dataloader:
+        for batch in tqdm.tqdm(val_dataloader):
             x = batch[0]
             x = x.to(device)
             x_prime = model(x)
@@ -82,7 +84,7 @@ def train_model(model, train_dataset, val_dataset, args, device):
 
     val_losses = []
     best_model = None
-    for epoch in tqdm.tqdm(range(args.num_epochs)):
+    for epoch in range(args.num_epochs):
         print(f"epoch {epoch + 1}")
         model, optimizer = train_epoch(
             model, optimizer, loss_fn, train_dataloader, device
@@ -116,7 +118,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dropout", help="dropout in the autoencoder", type=float, default=0.1
     )
-    parser.add("--num_epochs", help="number of epochs", type=int, default=10)
+    parser.add_argument(
+        "--num_epochs", help="number of epochs", type=int, default=10
+    )
     parser.add_argument("--save_path", help="save_path for the model")
     parser.add_argument("--seed", help="seed value", default=0, type=int)
 
@@ -129,6 +133,7 @@ if __name__ == "__main__":
     # read the train data
     input_train = ad.read_h5ad(args.train_file)
     data = input_train.X
+    data = data.todense()
 
     # split the train data into train and validation
     num_train = int(data.shape[0] * 0.9)
@@ -142,8 +147,8 @@ if __name__ == "__main__":
 
     # TODO: train the autoencoder (validation the performance with l2 loss
     # after every epoch)
-    train_dataset = TensorDataset(train_data)
-    val_dataset = TensorDataset(val_data)
+    train_dataset = TensorDataset((torch.Tensor(train_data)))
+    val_dataset = TensorDataset((torch.Tensor(val_data)))
 
     autoencoder = train_model(
         autoencoder, train_dataset, val_dataset, args, device
