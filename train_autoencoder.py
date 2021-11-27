d415ada5b311a4ab9381e0454d138bddf21290d8
Nihal Nayak <nihalnayak@gmail.com>
2021-11-27 14:09:27 -0500

    fix: remove the duplicate autoencoder

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -13,33 +13,9 @@ from torch.utils.data import DataLoader, TensorDataset
 
 from utils import set_seed
 from pred_autoencoder import autoencoder_reduce_dimension
+from models import AutoEncoder
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-# Reference: https://github.com/L1aoXingyu/pytorch-beginner/blob/master/08-AutoEncoder/simple_autoencoder.py
-class AutoEncoder(nn.Module):
-    def __init__(self, input_dim, reduced_dim, dropout=0.1) -> None:
-        super().__init__()
-
-        # adding a dropout makes it look like a denoising autoencoder.
-        self.encoder = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Linear(input_dim, 256),
-            nn.ReLU(True),
-            nn.Linear(256, reduced_dim),
-            nn.ReLU(True),
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(reduced_dim, 256),
-            nn.ReLU(True),
-            nn.Linear(256, input_dim),
-            nn.Tanh(),
-        )
-
-    def forward(self, x):
-        z = self.encoder(x)
-        x_prime = self.decoder(z)
-        return x_prime
 
 
 def train_epoch(model, optimizer, loss_fn, train_dataloader, device):
